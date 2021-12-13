f4b6be359c8096114bc4c6de14a586e14a4e288e
email <abe.solis2015@gmail.com>
2021-12-13 15:34:26 -0800

    Function for getting Notes

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,3 +1,12 @@
 const router = require('express').Router()
 const path = require('path')
 const fs = require('fs')
+
+//Function for gettng Notes
+
+router.get('/api/notes', (req, res) =>{
+  fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf-8', (err,data) =>{
+if (err) {console.log(err) }
+res.json(JSON.parse(data))
+  })
+})
