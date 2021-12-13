9450e65dd5ddad0b5915715f2fd2c37b9133079f
email <abe.solis2015@gmail.com>
2021-12-13 15:46:23 -0800

    stringify notes to post

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -10,3 +10,19 @@ if (err) {console.log(err) }
 res.json(JSON.parse(data))
   })
 })
+
+//Function for posting notes
+
+router.post('/api/notes', (req, res) =>{
+  const note = req.body
+  note.id = uuidv4();
+  fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf-8' , (err,data) =>{
+    if (err) { console.log(err)}
+    const notes = JSON.parse(data)
+    notes.push(note)
+    fs.writeFile(path.join(__dirname, '..', 'db' , 'db.json'), JSON.stringify(notes), err => {
+      if (err) {console.log(err)}
+      res.sendStatus(200)
+    })
+  })
+})
