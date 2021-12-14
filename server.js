7fb3ea0e75b08bf565b76c377c987070f9d05129
email <abe.solis2015@gmail.com>
2021-12-13 17:54:14 -0800

    updated port

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.get('/notes', (req, res) =>{
  })
 
 
-// const port = process.env.PORT || 4000;
-// app.listen(port);
+const port = process.env.PORT || 4000;
+app.listen(port);
 
 
-app.listen(3000)
+// app.listen(3000)
